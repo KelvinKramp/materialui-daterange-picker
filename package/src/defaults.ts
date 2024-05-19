3f6fdbde8ee447e8a398aad149666c70734dc263
Kelvin Kramp <k.h.kramp@gmail.com>
2024-05-19 21:23:53 +0200

    remove deprecated, update react

--- package/src/defaults.ts
+++ package/src/defaults.ts
@@ -12,7 +12,7 @@ import {
 
 // eslint-disable-next-line no-unused-vars
 import { DefinedRange } from './types';
-
+//  test
 const getDefaultRanges = (date: Date): DefinedRange[] => [
   {
     label: 'Today',
